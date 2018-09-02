dad140c4b3019e86a52e3eb1bfe841649fedb6a8
rodrigoqaz <rddsouzaoliveira@gmail.com>
2018-09-02 09:11:08 -0400

    teste

--- download_import.R
+++ download_import.R
@@ -1,3 +1,19 @@
 #### Download dos dados na página do Caged ####
 
 # Carrega os pacotes necessários:
+library(tidyverse)
+
+
+# Caminho para a base de dados:
+
+path <- "ftp://ftp.mtps.gov.br/pdet/microdados/CAGED/"
+
+ano <- 2017
+mes <- str_pad(seq(1:12),2, pad="0")
+files <- paste0("CAGEDEST_",mes, ano, ".7z")
+
+for (i in 1:12){
+  download.file(paste0(path,ano,"/",files[i]), files)
+}
+
+
