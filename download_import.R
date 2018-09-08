13bcce12c8484ecc6c18b28819090dc7f7ddb642
rodrigoqaz <rddsouzaoliveira@gmail.com>
2018-09-08 12:03:45 -0400

    download and import finished

--- download_import.R
+++ download_import.R
@@ -8,12 +8,26 @@ library(tidyverse)
 
 path <- "ftp://ftp.mtps.gov.br/pdet/microdados/CAGED/"
 
+# Parâmetros paa extração dos dados:
 ano <- 2017
 mes <- str_pad(seq(1:12),2, pad="0")
-files <- paste0("CAGEDEST_",mes, ano, ".7z")
+files <- paste0("CAGEDEST_", mes, ano, ".7z")
+dados <- NULL
 
+# Loop para fazer o download, extração e importação dos dados:
 for (i in 1:12){
-  download.file(paste0(path,ano,"/",files[i]), files)
+  #download:
+  download.file(url = paste0(path,ano,"/",files[i]), destfile = files[i], mode='wb', method='libcurl')
+  
+  #descompacta do arquivo. Deve ter instalado o 7Zip no pc e colocar o caminho para o executável abaixo
+  system(paste0('"C:\\Program Files\\7-Zip\\7z.exe" e ', files[i]))
+  
+  #importa e junta dos arquivos em uma único data_frame:
+  temp <- read_delim(file = str_replace(files[i], pattern = ".7z", replacement = ".txt"), delim = ";", locale = locale(encoding = "latin1"))
+  dados <- rbind(dados,temp)
+  rm(temp)
+  gc(reset = T) #limpa memoria
 }
 
-
+#salva o conjunto de dados em um arquivo rda:
+save(dados, file = "dados.rda")
